a3158ec703c946586181cd0d5a59a289ba941fd0
nahuel-aguero-itti <nahuel.aguero@itti.digital>
2025-04-17 21:23:05 -0300

    lista de productos funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react'
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import ListProducts from './pages/ListProducts'
 import Layout from './components/Layout'
-
+import Cart from './pages/CartDetail'
 import ProductsDetails from './pages/ProductsDetails'
+import AdminDashboard from './pages/AdminLayout'
 
-function App() { 
+function App() {
 
   return (
     <Router>
-        <Layout>
-      <Routes>
+      <Layout>
+        <Routes>
           <Route path="/" element={<ListProducts />} />
-          <Route path="/products/:id" element={<ProductsDetails />} /> 
-      </Routes>
-        </Layout>
+          <Route path="/products/:id" element={<ProductsDetails />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
