5aeda48432afa92a32136a27b32d6f3b7dec4750
wasabi <aldofranco991@gmail.com>
2025-05-16 16:39:12 -0300

    Fix de imagenes para el tamanho de las tazas

--- src/components/ui/CupButton.jsx
+++ src/components/ui/CupButton.jsx
@@ -1,8 +1,14 @@
 const CupButton = ({ imagen, setActiveCup }) => {
+  const imageClass = {
+    small: "bg-[url('/assets/small.svg')]",
+    medium: "bg-[url('/assets/medium.svg')]",
+    large: "bg-[url('/assets/large.svg')]",
+  }[imagen];
+
   return (
     <div
       onClick={() => setActiveCup(imagen)}
-      className={`mb-4 bg-[url('/assets/${imagen}.svg')] bg-no-repeat bg-auto bg-bottom cursor-pointer h-10 w-14 p-2 `}
+      className={`mb-4 ${imageClass} bg-no-repeat bg-auto bg-bottom cursor-pointer h-10 w-14 p-2 `}
     ></div>
   );
 };
