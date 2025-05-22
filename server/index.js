e80e77fc534c9547aaf091f69c96d89c998e5ee6
wasabi <aldofranco991@gmail.com>
2025-05-22 13:34:40 -0300

    Fix: se agrego los methods para solucionar el error cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // o dominio de Vercel
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 connectDB();
 
 app.use("/uploads", express.static("../public/uploads"));
